fc79abab3900cc4d5c12a5d73bccb9ba98ae11d0
Imran Jawaid <jawaidi@fcc.ca>
2017-09-22 15:32:57 -0600

    cleanup.
    cleanup

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-
-
 var index = require('./routes/index');
 var scrap = require('./routes/scrap');
 var file = require('./routes/file');
 
-
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +19,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);

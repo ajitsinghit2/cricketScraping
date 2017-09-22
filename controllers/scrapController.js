75291eaec540f8ea1b933cfe5417beab5fa59ac7
Imran Jawaid <jawaidi@fcc.ca>
2017-09-22 13:18:09 -0600

    refactor
    cleanup.

--- controllers/scrapController.js
+++ controllers/scrapController.js
@@ -24,6 +24,36 @@ function getURL(type, pageNo) {
         return urlT20;
 }
 
+function populateData(html) {
+    var $ = html;
+    var data = [];
+        $('table.engineTable').eq(2).filter(function () {
+        var rows = $(this).find("tr");
+        var index = 1;
+        if (rows.length > 1) {
+            rows.not("thead tr").each(function () {
+                var match = new summary();
+                var row = $(this);
+                var matchBetween = $(row).find("td").eq(5).text();
+
+                match.team1 = matchBetween.split("v")[0];
+                match.team2 = matchBetween.split("v")[1];
+                match.winner = $(row).find("td").eq(0).text();
+                match.margin = $(row).find("td").eq(2).text();
+                match.ballsLeft = $(row).find("td").eq(3).text();
+                match.date = $(row).find("td").eq(7).text();
+                match.matchNumber = counter;
+                match.groundId = $("#engine-dd" + index + " ul li:nth-child(1) a").attr("href");
+                match.scoreCardId = $("#engine-dd" + index + " ul li:nth-child(3) a").attr("href");
+                counter++;
+                index++;
+                data.push(match);
+            });
+        }
+    });
+        return data;
+}
+
 function ScrapSummary(pageNo, type, cb) {
 
     var url = getURL(type, pageNo);
@@ -31,35 +61,10 @@ function ScrapSummary(pageNo, type, cb) {
     request(url, function (error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
-            var json = [];
-
-            $('table.engineTable').eq(2).filter(function () {
-                var rows = $(this).find("tr");
-                var index = 1;
-                if (rows.length > 1) {
-                    rows.not("thead tr").each(function () {
-                        var match = new summary();
-                        var row = $(this);
-                        var matchBetween = $(row).find("td").eq(5).text();
-
-                        match.team1 = matchBetween.split("v")[0];
-                        match.team2 = matchBetween.split("v")[1];
-                        match.winner = $(row).find("td").eq(0).text();
-                        match.margin = $(row).find("td").eq(2).text();
-                        match.ballsLeft = $(row).find("td").eq(3).text();
-                        match.date = $(row).find("td").eq(7).text();
-                        match.matchNumber = counter;
-                        match.groundId = $("#engine-dd" + index + " ul li:nth-child(1) a").attr("href");
-                        match.scoreCardId = $("#engine-dd" + index + " ul li:nth-child(3) a").attr("href");
-                        counter++;
-                        index++;
-                        json.push(match);
-                    });
-                }
-            })
+            var data = populateData($);
         }
         //send data in callback
-        cb(pageNo, null, json);
+        cb(pageNo, null, data);
     });
 }
 
@@ -84,18 +89,21 @@ function scrap(totalPages, outputFilename) {
 }
 
 exports.scrapODI = function (req, res) {
-    scrap(81, "ODISummary");
+    var totalPages = 81;
+    scrap(totalPages, "ODISummary");
     res.send('Output stored in test file!');
 };
 
 exports.scrapTest = function (req, res) {
-    scrap(46, "TestSummary");
+    var totalPages = 46;
+    scrap(totalPages, "TestSummary");
     res.send('Output stored in test file!');
 };
 
 
 exports.scrapT20 = function (req, res) {
-    scrap(13, "T20Summary");
+    var totalPages = 13;
+    scrap(totalPages, "T20Summary");
     res.send('Output stored in test file!');
 };
 
